538ed9153f38f109700e1f03a6b37b8199496a4d
sbeins <sean.beins@gmail.com>
2019-03-19 08:23:09 -0500

    Make gallery changes

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,33 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  state = {
+    spans: 0
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10 + 1);
+
+    this.setState({ spans: spans });
+  };
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
-        <img alt={description} src={urls.small} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.small} />
       </div>
     );
   }
